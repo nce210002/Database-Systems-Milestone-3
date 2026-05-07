cab4808859e525846bd7722271daff9425895c22
nce210002 <nce210002@utdallas.edu>
2026-05-07 18:17:13 -0500

    Update GUI.py
    Got started on the intro page.

--- GUI.py
+++ GUI.py
@@ -1,9 +1,15 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("Airport System")
+root.geometry("400x200")  # Default window size
 
 # Intro GUI page
 label = tk.Label(root, text="Welcome to the Airport System! Select an activity.")
 label.pack()
+
+## Buttons for different activities
+button = tk.Button(root, text="Search Flight", width=25, command=root.destroy)
+button.pack()
 
 root.mainloop()
